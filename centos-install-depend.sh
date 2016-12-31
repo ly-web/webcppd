a053309f12cb3ebef2d4cb8db504824203ae3061
ubuntu1610 <ubuntu1610@ubuntu16.10>
2016-12-31 16:32:34 +0800

    完善install依赖

--- centos-install-depend.sh
+++ centos-install-depend.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-mkdir depend 
+if [ ! -d depend ];then
+    mkdir depend 
+fi;
 cd depend
-if [ !-e cmake-3.7.1.tar.gz ];then
+if [ ! -f cmake-3.7.1.tar.gz ];then
     wget https://cmake.org/files/v3.7/cmake-3.7.1.tar.gz
 fi
 tar xzfv cmake-3.7.1.tar.gz
@@ -13,15 +15,16 @@ sudo make install
 cd ..
 
 sudo yum -y install openssl-devel unixODBC-devel mariadb-devel
-if [ !-e poco-1.7.6-all.tar.gz ];then
+if [ ! -e poco-1.7.6-all.tar.gz ];then
     wget https://pocoproject.org/releases/poco-1.7.6/poco-1.7.6-all.tar.gz
 fi
 tar xzfv poco-1.7.6-all.tar.gz
 cd poco-1.7.6-all
-mkdir cmake_build
+if [ ! -d cmake_build ];then
+    mkdir cmake_build
+fi
 cd cmake_build
 cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=/usr ..
 make
 sudo make install
 sudo ldconfig
-
--- ubuntu-install-depend.sh
+++ ubuntu-install-depend.sh
@@ -2,15 +2,16 @@
 mkdir depend 
 cd depend
 sudo apt-get -y install openssl libssl-dev unixodbc-dev libmysqld-dev
-if [ !-e poco-1.7.6-all.tar.gz ];then
+if [ ! -e poco-1.7.6-all.tar.gz ];then
     wget https://pocoproject.org/releases/poco-1.7.6/poco-1.7.6-all.tar.gz
 fi
 tar xzfv poco-1.7.6-all.tar.gz
 cd poco-1.7.6-all
-mkdir cmake_build
+if [ ! -d cmake_build ];then
+    mkdir cmake_build
+fi
 cd cmake_build
 cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=/usr ..
 make
 sudo make install
 sudo ldconfig
-cd ../../../
